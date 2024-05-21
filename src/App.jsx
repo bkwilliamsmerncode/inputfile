06356d7bfa45d19b9d5eb2a3cde6908edabda8eb
Brian Williams <152632135+bkwilliamsmerncode@users.noreply.github.com>
2024-05-21 12:12:48 -0700

    1st commit
    5/21/2024

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="main">
+        <div className="box">
+          <div className="btn">
+            ADD PHOTO
+            <div className="input">
+              <input type="file" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
